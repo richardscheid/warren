98d2c1454a22ffdf0b8c17b0d92800a2e14ea4fb
richardscheid <rscheid@live.com>
2020-08-15 18:10:42 -0300

    feat: create payment page

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+// import Rescue from './pages/Rescue';
+import Deposit from './pages/Deposit';
+import Payment from './pages/Payment';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={Home} />
+        <Route path='/home' exact component={Home} />
+        {/* <Route path='/rescue' component={Rescue} />*/}
+        <Route path='/payment' component={Payment} />
+        <Route path='/Deposit' component={Deposit} />
       </Switch>
     </BrowserRouter>
   );
