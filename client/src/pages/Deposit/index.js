ea7aaaf1716ff658efc1ee31dae68a7ef009a136
richardscheid <rscheid@live.com>
2020-08-15 16:43:31 -0300

    feat: create deposit page

---
+++ client/src/pages/Deposit/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+export default function Deposit({ history }) {
+  const account = getAccount(history);
+
+  const [amount, setAmount] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    await api.post('/transactions', { amount, type: 2 });
+
+    history.push('/home');
+  }
+
+  function getAccount({ location: { state } }) {
+    return state ? state.account : '';
+  }
+
+  return (
+    <>
+      <div className='home-container'>
+        <header>
+          <h1>Depósito</h1>
+        </header>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          id='amount'
+          placeholder='Valor a ser depositado'
+          required
+          autoComplete='off'
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
+
+        <div className='button-content'>
+          <Link to='/home'>
+            <button className='btn'>Voltar</button>
+          </Link>
+
+          <button className='btn' type='submit'>
+            Salvar
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
