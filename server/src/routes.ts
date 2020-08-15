a9a4dffae3a09b2e6ec1fc93ae42b947795bbfa7
richardscheid <rscheid@live.com>
2020-08-15 13:00:38 -0300

    chore: create account route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
+import AccountController from './controllers/AccountController';
 import TransactionController from './controllers/TransactionController';
 
 const routes = Router();
+
+routes.get('/accounts', AccountController.find);
 
 routes.get('/transactions', TransactionController.list);
 routes.post('/transactions', TransactionController.execute);
