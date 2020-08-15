abbc6520ea7efb5ffd9023df7aa45d52e466efe8
richardscheid <rscheid@live.com>
2020-08-15 12:43:13 -0300

    chore: create application

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='content'>
+        <Routes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
