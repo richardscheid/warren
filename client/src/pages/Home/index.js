603969217ffcb8ec1203fb2e166b8b07c18af7bc
richardscheid <rscheid@live.com>
2020-08-15 15:47:42 -0300

    feat: add balance

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,27 +7,33 @@ import api from '../../services/api';
 import './styles.css';
 
 export default function Dashboard({ history }) {
+  const [account, setAccount] = useState('');
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     async function loadTransactions() {
       const response = await api.get('/transactions');
-      console.log(response.data);
       setTransactions(response.data);
     }
 
     loadTransactions();
   }, []);
 
-  function handleEdit(transaction) {
-    history.push({
-      pathname: '/transaction',
-      state: { detail: transaction },
-    });
-  }
+  useEffect(() => {
+    async function loadAccount() {
+      const response = await api.get('/accounts');
+      setAccount(response.data);
+    }
+
+    loadAccount();
+  });
 
   function getTransactionType(type) {
     return type === 1 ? 'Resgate' : type === 2 ? 'Depósito' : 'Pagamento';
+  }
+
+  function formatNumber(amount) {
+    return new Intl.NumberFormat().format(amount);
   }
 
   return (
@@ -38,17 +44,24 @@ export default function Dashboard({ history }) {
         </header>
       </div>
 
+      <div className='balance-container'>
+        <p>Saldo: </p>
+        <p>{`R$ ${formatNumber(account.balance)}`}</p>
+      </div>
+
       <ul className='home-list'>
         {transactions.map((transaction) => (
-          <li key={transaction._id} onClick={() => handleEdit(transaction)}>
-            <p className='place'>{getTransactionType(transaction.type)}</p>
+          <li key={transaction._id}>
+            <p className='type'>{getTransactionType(transaction.type)}</p>
             <ul>
               <li className='bottom-text'>
                 <p className='date'>{`${moment(transaction.createdAt).format(
                   'DD/MM/YYYY HH:mm'
                 )}`}</p>
                 <p className='split'></p>
-                <p className='amout'>{`R$ ${transaction.amount ?? ' 0'}`}</p>
+                <p className='amount'>{`R$ ${
+                  formatNumber(transaction.amount) ?? ' 0'
+                }`}</p>
               </li>
             </ul>
           </li>
