0ec10876ef6631696e97fc5196b7922ea3601157
richardscheid <rscheid@live.com>
2020-08-16 18:06:18 -0300

    chore: add and configure jest

---
+++ server/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@builders/(.*)$': '<rootDir>/src/builders/$1',
+    '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+  },
+};
