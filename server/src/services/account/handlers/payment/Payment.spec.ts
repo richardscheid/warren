406f5ecafdcc14c97c533d5c15e2d5914690285a
richardscheid <rscheid@live.com>
2020-08-16 18:18:13 -0300

    refact: test description

--- server/src/services/account/handlers/payment/Payment.spec.ts
+++ server/src/services/account/handlers/payment/Payment.spec.ts
@@ -2,17 +2,17 @@ import { Transaction } from '@interfaces/ITransaction';
 import { AccountBuilder } from '@builders/AccountBuilder';
 import { TransactionBuilder } from '@builders/TransactionBuilder';
 
-import DepositHandler from './Payment';
+import PaymentHandler from './Payment';
 
-describe('Make a deposit', () => {
-  it('should make a deposit', () => {
+describe('Make a payment', () => {
+  it('should make a payment', () => {
     const account = new AccountBuilder().balance(20).build();
     const transaction = new TransactionBuilder()
       .type(Transaction.Payment)
       .amount(10)
       .build();
 
-    const balance = DepositHandler.process(account, transaction);
+    const balance = PaymentHandler.process(account, transaction);
 
     expect(balance).toBe(10);
   });
--- server/src/services/account/handlers/rescue/Rescue.spec.ts
+++ server/src/services/account/handlers/rescue/Rescue.spec.ts
@@ -2,17 +2,17 @@ import { Transaction } from '@interfaces/ITransaction';
 import { AccountBuilder } from '@builders/AccountBuilder';
 import { TransactionBuilder } from '@builders/TransactionBuilder';
 
-import DepositHandler from './Rescue';
+import RescueHandler from './Rescue';
 
-describe('Make a deposit', () => {
-  it('should make a deposit', () => {
+describe('Make a rescue', () => {
+  it('should make a rescue', () => {
     const account = new AccountBuilder().balance(50).build();
     const transaction = new TransactionBuilder()
       .type(Transaction.Rescue)
       .amount(40)
       .build();
 
-    const balance = DepositHandler.process(account, transaction);
+    const balance = RescueHandler.process(account, transaction);
 
     expect(balance).toBe(10);
   });
