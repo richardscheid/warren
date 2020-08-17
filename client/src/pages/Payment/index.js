325e5155ad7ed46e8f94d614943231b5bee78b60
richardscheid <rscheid@live.com>
2020-08-16 23:34:18 -0300

    refact: organizing styles

--- client/src/pages/Payment/index.js
+++ client/src/pages/Payment/index.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import logo from '../../assets/warren-logo.png';
 
-import './styles.css';
-
 export default function Payment({ history }) {
   const account = getAccount(history);
 
